47e42727bc7919d0a378523aa67a269cd8ea28ed
marnixah <marnix@hagehoeve.com>
2021-04-02 17:05:36 +0200

    feat: added markdown parsing of web.md

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const http = require('http');
+const showdown = require('showdown')
+const converter = new showdown.Converter()
+const fs = require('fs');
 
 const port = process.env.PORT || 1234;
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end('Hello, World!');
+    const content = fs.readFileSync('./web.md', "utf8");
+    res.end(converter.makeHtml(content));
 }
 
 const server = http.createServer(requestListener);
